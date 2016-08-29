f78d34bf11bdc9736540192ac686cb73727cb6d2
Nelson Rodriguez <nelsonhenge@hotmail.com>
2016-08-29 15:56:05 -0500

    Initial commit

---
+++ php/mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("DATABASE", "webservice"); // Nombre de la base de datos
+define("USERNAME", "root"); // Nombre del usuario
+define("PASSWORD", "irf840"); // Nombre de la constraseña
+?>
